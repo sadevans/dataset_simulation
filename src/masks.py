506b84f77a78720c04f9baa8ddde683bcd78eb24
sasha <a.s.gorokhova@yandex.ru>
2023-12-15 18:43:54 +0300

    сделала формирование семантических масок из бинарных

--- src/masks.py
+++ src/masks.py
@@ -7,15 +7,13 @@ import scipy
 import random
 import skimage.morphology as morphology
 from pathlib import Path
+import itertools
 
 
 def detect_contour(img):
     cont, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
     cont_image = np.zeros_like(img)
     cv2.drawContours(cont_image, cont, -1, 255, 0)
-    plt.imshow(cont_image) 
-    
     return cont, cont_image
 
 
@@ -47,60 +45,131 @@ def read_semantic_masks():
             print(f"Папка '{new_folder_name}' успешно создана по пути '{raw_path}'.")
         except OSError as error:
             print(f"Ошибка при создании папки: {error}")
-
-    
-
     
     filenames_masks = os.listdir(folder_path)
 
     return filenames_masks, signal_path, raw_path, folder_path
 
 
+def edit_bin_mask(mask):
+    copy = mask.copy()
+    mask_bool = mask >= 128
+    mask[mask_bool] = 255
+    mask[copy < 128] = 0
+
+    kernel = np.ones((5, 5), np.uint8)  # Можно изменить размер ядра по желанию
+    mask = ~mask
+
+    opening = np.clip((morphology.remove_small_holes(mask, 10)), 0, 1) * 255
+    result = opening.copy()
+    result[opening == 0] = 255
+    result[opening == 255] = 0
+    return result.astype(np.uint8)
 
 
-
-if __name__ == '__main__':
-    folder_path = input("Введите путь к папке c бинарными масками: ")
-
-    if os.path.exists(folder_path):
-        parent_directory = os.path.dirname(folder_path)
-        new_folder_name = input("Введите имя новой папки для семантических масок: ")
-        semantic_path = os.path.join(parent_directory, new_folder_name)
-
-        try:
-            os.mkdir(semantic_path)
-            print(f"Папка '{new_folder_name}' успешно создана по пути '{semantic_path}'.")
-        except OSError as error:
-            print(f"Ошибка при создании папки: {error}")
-
-        new_folder_name = input("Введите имя новой папки для шумных снимков: ")
-        raw_path = os.path.join(parent_directory, new_folder_name)
-
-        try:
-            os.mkdir(raw_path)
-            print(f"Папка '{new_folder_name}' успешно создана по пути '{raw_path}'.")
-        except OSError as error:
-            print(f"Ошибка при создании папки: {error}")
-
-    else:
-        print(f"Путь  не существует.")
-
-    
+def make_sem_from_bin(folder_path, semantic_path):
     filenames_masks = os.listdir(folder_path)
+    for file in filenames_masks:
+        bin_mask = cv2.imread(os.path.join(folder_path, file), 0)
+        bin_mask = edit_bin_mask(bin_mask)
+        cont, cont_image = detect_contour(np.clip(bin_mask, 0, 1)) # обанружила все контура
+        cont_array = [] # массив для всех точек всех новых контуров
+        for contour in cont:
+            # print('im here')
+            intersect = True
+            # offset = 20
+            while intersect:
+                offset = random.randint(3, 20) # выбор первочначальной ширины контура
+                # print('offset now ', offset)
+                temp = np.zeros_like(bin_mask) # создание временной картинки
+                cv2.drawContours(temp, [contour], -1, 128, offset) # рисование контура с первоначальным offset
+                c, _ = cv2.findContours(np.clip(temp, 0, 1), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # детектирование контура
+                temp = np.zeros_like(bin_mask) # опять создание временной картинки
+                cv2.drawContours(temp, c, 0, 128, 0) # рисование большого контура
+                nonzero = np.argwhere(temp > 0) # нахождение всех пикселей конутра
+                plt.imshow(temp)
+                nonzero = np.array([list(reversed(nonz)) for nonz in nonzero]) # создание массива со всеми точками
 
-    for file_name in filenames_masks:
-        bin_mask = cv2.imread(os.path.join(folder_path, file_name), 0)
-        
-        cont, cont_image = detect_contour(bin_mask)
-        for c in cont:
-            offset = random.randint(3, 20)
-            cv2.drawContours(cont_image, c, -1, 128, offset)
-            
-        # cv2.drawContours(cont_image, dst_np, -1, 128, -1)
+                # intersect = True
+                if len(cont_array) == 0: 
+                    cont_array.append(nonzero)
+                    intersect = False
+                else:
+                    # while not intersect:
+                    for c in cont_array:
+                        intersect_ = np.array([x for x in set(tuple(x) for x in nonzero) & set(tuple(x) for x in c)])
+                        distances = np.linalg.norm(c[:, np.newaxis, :] - nonzero, axis=2)
+                        # print(distances)
+                        close_points = np.where(distances <= 1)
+                        # print('close points', close_points[0])
+                        # print(len(close_points[0]))
+                        if len(intersect_) == 0 and len(close_points[0]) == 0:
+                            # print('final offset')
+                            intersect = False
+                            offset = offset
+            cv2.drawContours(cont_image, [contour], -1, 128, offset)
         cv2.drawContours(cont_image, cont, -1, 255, -1)
-
-        cv2.imwrite(os.path.join(semantic_path, file_name), cont_image.astype(np.uint8))
-
         plt.imshow(cont_image)
+        cv2.imwrite(os.path.join(semantic_path, file), cont_image.astype(np.uint8))
 
-    
+        # plt.show()
+
+
+
+# new new main
+if __name__ == '__main__':
+    print('main')
+    # folder_path = input("Введите путь к папке c бинарными масками: ")
+    folder_path = './data/bin_masks'
+    parent_directory = os.path.dirname(folder_path)
+    semantic_path = os.path.join(parent_directory, 'sem')
+    os.makedirs(semantic_path, exist_ok=True)
+    # print(folder_path, parent_directory, semantic_path)
+    filenames_masks = os.listdir(folder_path)
+    # print(filenames_masks)
+    make_sem_from_bin(folder_path, semantic_path)
+    # for file in filenames_masks:
+    #     bin_mask = cv2.imread(os.path.join(folder_path, file), 0)
+    #     bin_mask = edit_bin_mask(bin_mask)
+    #     print(np.unique(bin_mask))
+    #     cont, cont_image = detect_contour(np.clip(bin_mask, 0, 1)) # обанружила все контура
+    #     cont_array = [] # массив для всех точек всех новых контуров
+    #     for contour in cont:
+    #         # print('im here')
+    #         intersect = True
+    #         # offset = 20
+    #         while intersect:
+    #             offset = random.randint(3, 20) # выбор первочначальной ширины контура
+    #             # print('offset now ', offset)
+    #             temp = np.zeros_like(bin_mask) # создание временной картинки
+    #             cv2.drawContours(temp, [contour], -1, 128, offset) # рисование контура с первоначальным offset
+    #             c, _ = cv2.findContours(np.clip(temp, 0, 1), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # детектирование контура
+    #             temp = np.zeros_like(bin_mask) # опять создание временной картинки
+    #             cv2.drawContours(temp, c, 0, 128, 0) # рисование большого контура
+    #             nonzero = np.argwhere(temp > 0) # нахождение всех пикселей конутра
+    #             plt.imshow(temp)
+    #             nonzero = np.array([list(reversed(nonz)) for nonz in nonzero]) # создание массива со всеми точками
+
+    #             # intersect = True
+    #             if len(cont_array) == 0: 
+    #                 cont_array.append(nonzero)
+    #                 intersect = False
+    #             else:
+    #                 # while not intersect:
+    #                 for c in cont_array:
+    #                     intersect_ = np.array([x for x in set(tuple(x) for x in nonzero) & set(tuple(x) for x in c)])
+    #                     distances = np.linalg.norm(c[:, np.newaxis, :] - nonzero, axis=2)
+    #                     # print(distances)
+    #                     close_points = np.where(distances <= 1)
+    #                     # print('close points', close_points[0])
+    #                     # print(len(close_points[0]))
+    #                     if len(intersect_) == 0 and len(close_points[0]) == 0:
+    #                         # print('final offset')
+    #                         intersect = False
+    #                         offset = offset
+    #         cv2.drawContours(cont_image, [contour], -1, 128, offset)
+    #     cv2.drawContours(cont_image, cont, -1, 255, -1)
+    #     plt.imshow(cont_image)
+    #     cv2.imwrite(os.path.join(semantic_path, filenames_masks[0]), cont_image.astype(np.uint8))
+
+    #     plt.show()
--- test.py
+++ test.py
@@ -9,29 +9,6 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 
-# def formula_second1(img, angles, color_map, k, save_dir, file_name):
-#     print(np.unique(img))
-#     signal = np.zeros_like(img, dtype=np.float32)
-#     alpha_bord = angles[img == 128]
-#     # alpha_bord[alpha_bord==alpha_bord.min()] = np.radians(1)
-#     alpha_back = angles[img == 0]
-#     print(np.unique(alpha_back))
-#     alpha_hole = angles[img == 255]
-#     # print(np.unique(alpha_hole), np.unique(alpha_back))
-#     # k = k * 
-#     signal[img == 0] = (k*(1/(np.abs(np.cos(np.radians(alpha_back + 1)))**(0.87)) - 1) + 1) * color_map[img==0]
-#     signal[img == 128] = (k * (1/(np.abs(np.cos(np.radians(90)-(np.radians(180 - 90) - alpha_bord)))**(0.87)) - 1) + 1) *color_map[img==128]
-#     signal[img == 255] = (k * (1 / (np.abs(np.cos(np.radians(alpha_hole + 1)))**(1.1)) - 1) + 1) * color_map[img==255]
-#     signal = np.clip(signal, 0, 255)
-#     # print(np.unique(signal))
-#     # signal = cv2.GaussianBlur(signal, (11,11), 0)
-#     signal = cv2.GaussianBlur(signal, (9,9), 0)
-#     print(np.unique(signal))
-#     fig, ax = plt.subplots(1, 1, figsize=(30, 15))
-#     ax.imshow(signal)
-#     cv2.imwrite(f'{save_dir}/{file_name}', signal.astype(np.uint8))
-#     return signal
-
 def formula_second1(img, angles, color_map, k, save_dir, file_name):
     print(f'{save_dir}/{file_name}')
     signal = np.zeros_like(img, dtype=np.float32)
@@ -70,7 +47,7 @@ if __name__ == '__main__':
     b = 10
 
     # for debug
-    folder_path = '/home/sasha/WSLProjects/sem_simulator/simulate/data/semantic'
+    folder_path = './data/semantic'
     parent_directory = os.path.dirname(folder_path)
     
     signal_path = os.path.join(parent_directory, 'signal')
